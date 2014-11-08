5e4765a90c15626a4f0804ee765faac2c3ada917
maraoks <maranugraha26@gmail.com>
2014-11-08 17:40:17 +0700

    add reoutes

--- config/routes.php
+++ config/routes.php
@@ -6,44 +6,13 @@
 */
 
 $ROUTES = array(
-<<<<<<< HEAD
-		'home',
-		'contact',
-		'upload',
-                'zip', 
-                'login',
-                'onebyone',
-                'browse',
-                'user',
-                'activate',
-                'register',
-                'setting',
-				'article',
-                'uploadfoto',
-                'getphoto',
-				'about_us',
-				'produk',
-				'company','monthsnews','lab_training','field_training','rental'
-=======
+
 				'home',
 				'company',
 				'about_us',
 				'produk',
-				'about',
-				'partner',
 				'master_template',
 				'contact',
 				'search',
-				'specimen',
-				'tulisan',
-				'testing',
-				'news',
-				'upload',
-				'contact',
-				'monthsnews',
-				'lab_training',
-				'field_training',
-				'rental'
->>>>>>> bb7a389ad548a4b91234d9ec363bc3fd827a1d14
                 );
 ?>
