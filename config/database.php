1bd8f5419bf50b03268c84af41410567eeb61595
pukiscuy <pukiscuy@gmail.com>
2014-11-10 06:40:07 +0700

    public_assets

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+/* Database Config */
+
+$dbConfig[0]['host'] = "127.0.0.1";
+$dbConfig[0]['user'] = "root";
+$dbConfig[0]['pass'] = "";
+$dbConfig[0]['name'] = "db_ptmitra";
+$dbConfig[0]['server'] = "mysql";
+
+$dbConfig[1]['host'] = "127.0.0.1";
+$dbConfig[1]['user'] = "root";
+$dbConfig[1]['pass'] = "";
+$dbConfig[1]['name'] = "db_ptmitra";
+$dbConfig[1]['server'] = "mysql";
+
+/* dont touch this */
+$DBCOLL = $dbConfig[0]['name'];
+$DBAPP = $dbConfig[1]['name'];
+
+?>
