0ec9db81e518a855be0ab0526a2607134ff535bb
pukiscuy <pukiscuy@gmail.com>
2014-12-02 04:58:53 +0700

    home

--- app/controller/home.php
+++ app/controller/home.php
@@ -49,6 +49,22 @@ class home extends Controller {
 
 		return $this->loadView('home');
 	}
+	public function visi(){
+		global $basedomain;
+		global $baseheader;
+		$result_data = $this->models->about();
+		$result_data_file3 = $this->models->geophysics();
+		$result_data_file4 = $this->models->scientifics();
+		$result_data_file5 = $this->models->civil();
+		$this->view->assign('data4',$result_data_file5);
+		$var = array(1,2,3);
+		$this->view->assign('data',$result_data);
+		$this->view->assign('coba','coba data smarty');
+		$this->view->assign('data2',$result_data_file3);
+		$this->view->assign('data3',$result_data_file4);
+
+		return $this->loadView('home');
+	}
 	public function about_us(){
 		global $basedomain;
 		global $baseheader;
