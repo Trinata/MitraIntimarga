6c11bd57e9c5e9b2ba6236161747b1dd02ed5971
Ovan Pulu <ovan89@gmail.com>
2014-12-02 09:26:42 +0700

    route sama controller menu

---
+++ config/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+	list menu yang akan dipanggil oleh APP 
+	hanya class yang ada di routes dibawah ini yang dijalankan oleh aplikasi
+*/
+
+$ROUTES = array(
+
+
+				'home',
+				'company',
+				'about_us',
+				'produk',
+				'master_template',
+				'contact',
+				'search',
+				'news',
+				'upload',
+				'contact',
+				'monthsnews',
+				'lab_training',
+				'field_training',
+				'rental',
+				'brocure',
+				'album',
+				'music'
+
+                );
+
+?>
